cd82381170abe65317084c751fd4829c46395f17
Peter Ding <peterding@augmentum.com.cn>
2014-08-19 14:44:13 +0800

    refine fragment demo

--- Sample/src/com/dk/sample/FragmentDemo.java
+++ Sample/src/com/dk/sample/FragmentDemo.java
@@ -1,16 +1,20 @@
 package com.dk.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 
 import com.dk.animation.SwitchAnimationUtil;
+import com.dk.animation.SwitchAnimationUtil.AnimationType;
 
 public class FragmentDemo extends FragmentActivity {
     private FragmentManager mFragmentManager;
@@ -24,6 +28,47 @@ public class FragmentDemo extends FragmentActivity {
         FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.add(R.id.fragment_container, new FlowerFragment());
         mFragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_alpha:
+            Constant.mType = AnimationType.ALPHA;
+            break;
+        case R.id.action_flip_horizon:
+            Constant.mType = AnimationType.FLIP_HORIZON;
+            break;
+        case R.id.action_flip_vertical:
+            Constant.mType = AnimationType.FLIP_VERTICAL;
+            break;
+        case R.id.action_horizon_left:
+            Constant.mType = AnimationType.HORIZION_LEFT;
+            break;
+        case R.id.action_horizon_right:
+            Constant.mType = AnimationType.HORIZION_RIGHT;
+            break;
+        case R.id.action_rotate:
+            Constant.mType = AnimationType.ROTATE;
+            break;
+        case R.id.action_scale:
+            Constant.mType = AnimationType.SCALE;
+            break;
+        case R.id.action_cross:
+            Constant.mType = AnimationType.HORIZON_CROSS;
+            break;
+        case R.id.action_next:
+            break;
+        }
+        Intent intent = new Intent(FragmentDemo.this, MainActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 
 }
